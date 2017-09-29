32ac59169ec344b953861ffe637fb749070af461
Tzer Wong <tzer@mit.edu>
2017-09-28 22:54:53 -0400

    P1 updated

--- code/P1/loadFittingDataP1.py
+++ code/P1/loadFittingDataP1.py
@@ -1,10 +1,9 @@
 import pylab as pl
 import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
+
 
 def getData():
-    
+
     # load the fitting data for X and y and return as elements of a tuple
     # X is a 100 by 10 matrix and y is a vector of length 100
     # Each corresponding row for X and y represents a single data sample
@@ -12,5 +11,41 @@ def getData():
     X = pl.loadtxt('fittingdatap1_x.txt')
     y = pl.loadtxt('fittingdatap1_y.txt')
 
-    return (X,y) 
+    return (X,y)
 
+X, y = getData()
+
+def gradient_descent(thresh, learning_rate):
+    old_theta = np.array([0]*len(X[0]))
+    new_theta = old_theta - learning_rate*(2*(np.dot(np.transpose(X), np.dot(X, old_theta) - y)))
+    step = 0
+    while np.linalg.norm(new_theta - old_theta) > thresh:
+        old_theta = new_theta
+        new_theta = old_theta - learning_rate*(2*(np.dot(np.transpose(X), np.dot(X, old_theta) - y)))
+        step += 1
+    return (new_theta, step)
+
+def sgd(thresh, learning_rate):
+    old_theta = np.array([0]*len(X[0]))
+    new_theta = old_theta - 2*learning_rate*(np.dot(X[0], (np.dot(X[0], old_theta) - y[0])))
+    i = 0
+    step = 0
+    while np.linalg.norm(new_theta-old_theta) > thresh:
+        step += 1
+        old_theta = new_theta
+        ind = i%len(X)
+        new_theta = old_theta - 2*learning_rate*(np.dot(X[ind], (np.dot(X[ind], old_theta) - y[ind])))
+        i += 1
+    return (new_theta, step)
+
+gd_ans = gradient_descent(0.0000011, 0.000000025)
+print('gradient_descent: ', gd_ans[0], 'steps: ',gd_ans[1] )
+
+real_answer = np.dot(np.linalg.inv(np.dot(np.transpose(X), X)), np.dot(np.transpose(X), y))
+print('real_answer--->', real_answer)
+
+
+
+sgd_ans = sgd(0.000000011, 0.00000028)
+print('sgd: ', sgd_ans[0], 'steps: ', sgd_ans[1])
+print('difference_____ >', real_answer - sgd_ans[0])
--- code/P1/loadParametersP1.py
+++ code/P1/loadParametersP1.py
@@ -50,14 +50,21 @@ def gauss_grad_descent(f, df, start, thresh, step_size):
     ws.append(new_w)
     return new_w, norms, ws
 
-def do_grad_gauss():
-    start = [5,5]
-    thresh = .000001
-    step_size = .25
+def do_grad_gauss(start, thresh, step_size, plotNorm = False):
+    # start = [5,5]
+    # thresh = .000001
+    # step_size = .25
     end, norms, ws = gauss_grad_descent(gauss, gauss_derivative, start, thresh, step_size)
     print("GD Gauss: ", end)
-    plt.plot(norms)
-    plt.show()
+    if plotNorm:
+        plt.plot(norms)
+        plt.xlabel("Iterations")
+        plt.ylabel("Norm of Gradient")
+        plt.title("Evolution of Gaussian Gradient Norm (S = " + str(start) + ")")
+        plt.show()
+        # plt.plot(ws)
+        # plt.show()
+    return end, norms, ws
 
 def bowl(A, b, x):
     return 0.5*np.dot(np.dot(np.transpose(x), A), x) - np.dot(np.transpose(x), b)
@@ -109,10 +116,149 @@ def approx_grad_gauss(f, x, d):
         approx_grad.append((f(mean, cov, x+y)-f(mean, cov, x))/d[i])
     return np.array(approx_grad)
 
-delta = 0.001
-for i in np.arange(9,11.25,0.25):
-    for j in np.arange(9,11.25,0.25):
-        point = np.array([i,j])
-        print("APPROX: ", approx_grad_gauss(gauss, point, np.array([delta]*len(point))))
-        print("ACTUAL: ", gauss_derivative(gauss, mean, cov, np.array([i,j])))
-        print("------------------------------------")
+# delta = 0.001
+# for i in np.arange(9,11.25,0.25):
+#     for j in np.arange(9,11.25,0.25):
+#         point = np.array([i,j])
+#         print("APPROX: ", approx_grad_gauss(gauss, point, np.array([delta]*len(point))))
+#         print("ACTUAL: ", gauss_derivative(gauss, mean, cov, np.array([i,j])))
+#         print("------------------------------------")
+
+
+
+# print()
+
+#
+# l1 = []
+# l2 = []
+# lD = []
+# l0 = []
+# for i in np.arange(80/3-5,80/3+8.1,0.1):
+#     v = np.array([i,i])
+#     l1.append(v)
+#     l2.append(bowl(A,b, v))
+#     l0.append(0)
+#     lD.append(bowl_derivative( A,b, v))
+#
+# # plt.plot(l1, l2, "bo")
+# plt.plot(l1, lD, "r^")
+# plt.plot(l1, l0, "g-")
+# plt.show()
+#
+#
+
+#
+
+def gen_gauss_plots():
+    # start = [5,5]
+    # thresh = .000001
+    # step_size = .25
+    #
+    # end, norms, ws = do_grad_gauss(start, thresh, step_size, False)
+
+    ends_vary_start = []
+    ends_vary_step = []
+    ends_vary_thresh = []
+
+    for i,j in [[k]*2 for k in range(4, 10)]:
+        start = [i,j]
+        thresh = .0001
+        step_size = .25
+        end, norms, ws = do_grad_gauss(start, thresh, step_size, False)
+        ends_vary_start.append((end, np.linalg.norm([10-i,10-j])))
+
+    ends_only_start_dist_end = [(10-ends_vary_start[i][0][0])*1.414 for i in range(len(ends_vary_start))]
+    ends_only_start_dist_start = [ends_vary_start[i][1] for i in range(len(ends_vary_start))]
+    for i in range(len(ends_only_start_dist_start)):
+        print("-----")
+        print(ends_only_start_dist_start)
+        print("-----")
+        print(ends_only_start_dist_end)
+        print("-----")
+        plt.plot(ends_only_start_dist_start[i], ends_only_start_dist_end[i], ls = "None", color = str((i+1.0)/len(ends_only_start_dist_start)), marker = "o")
+    plt.xlabel("Initial Distance from Minimum")
+    plt.ylabel("End Distance from Minimum")
+    plt.title("Distance from Minimum Over Starting Distances " + "(thresh = " + str(thresh) + ", step size = " + str(step_size) + ")")
+    plt.show()
+
+    for thresh in [.01, .03, .001, .003, .0001, .003, .00001]:
+        start = [7,7]
+        # thresh = .0001
+        step_size = .25
+
+        end, norms, ws = do_grad_gauss(start, thresh, step_size, False)
+        ends_vary_thresh.append((end, -math.log(thresh)))
+
+    ends_only_thresh_dist_end = [(10-ends_vary_thresh[i][0][0])*1.414 for i in range(len(ends_vary_thresh))]
+    ends_only_thresh_val = [ends_vary_thresh[i][1] for i in range(len(ends_vary_thresh))]
+    for i in range(len(ends_only_thresh_dist_end)):
+        plt.plot(ends_only_thresh_val[i], ends_only_thresh_dist_end[i], ls = "None", color = str((i+1.0)/len(ends_only_thresh_val)), marker = "o")
+    plt.xlabel("-log(threshold)")
+    plt.ylabel("End Distance from Minimum")
+    plt.title("Distance from Minimum Over Thresholds " + "(step size = " + str(step_size) + ", start= " + str(start) + ")")
+    plt.show()
+
+    for step_size in [1, .25, .1, .05, .025, .01]:
+        start = [7,7]
+        thresh = .0001
+        # step_size = .25
+
+        end, norms, ws = do_grad_gauss(start, thresh, step_size, False)
+        ends_vary_step.append((end, step_size))
+
+    ends_only_step_dist_end = [(10-ends_vary_step[i][0][0])*1.414 for i in range(len(ends_vary_step))]
+    ends_only_step_val = [ends_vary_step[i][1] for i in range(len(ends_vary_step))]
+    for i in range(len(ends_only_step_dist_end)):
+        plt.plot(ends_only_step_val[i], ends_only_step_dist_end[i], ls = "None", color = str((i+1.0)/len(ends_only_step_val)), marker = "o")
+    plt.xlabel("Step Size")
+    plt.ylabel("End Distance from Minimum")
+    plt.title("Distance from Minimum Over Step Sizes " + "(thresh = " + str(thresh) + ", start= " + str(start) + ")")
+    plt.show()
+
+    # ends_only_start_x = [ends_vary_start[i][0][0] for i in range(len(ends_vary_start))]
+    # ends_only_start_y = [ends_vary_start[i][0][1] for i in range(len(ends_vary_start))]
+    # for i in range(len(ends_only_start_x)):
+    #     plt.plot(ends_only_start_x[i], ends_only_start_y[i], ls = "None", color = str((i+1.0)/len(ends_only_start_x)), marker = "o")
+    # plt.xlabel("End x Value")
+    # plt.ylabel("End y Value")
+    # plt.title("Ending Point Based on Varying Start (darker = closer)")
+    # plt.show()
+
+    # ends_only_start_dist_end = [(10-ends_vary_start[i][0][0])*1.414 for i in range(len(ends_vary_start))]
+    # ends_only_start_dist_start = [ends_vary_start[i][1] for i in range(len(ends_vary_start))]
+    # for i in range(len(ends_only_start_dist_start)):
+    #     print("-----")
+    #     print(ends_only_start_dist_start)
+    #     print("-----")
+    #     print(ends_only_start_dist_end)
+    #     print("-----")
+    #     plt.plot(ends_only_start_dist_start[i], ends_only_start_dist_end[i], ls = "None", color = str((i+1.0)/len(ends_only_start_dist_start)), marker = "o")
+    # plt.xlabel("Initial Distance from Minimum")
+    # plt.ylabel("End Distance from Minimum")
+    # plt.title("Distance from Minimum Over Starting Distances " + "(thresh = " + str(thresh) + ", step size = " + str(step_size) + ")")
+    # plt.show()
+
+
+# gen_gauss_plots()
+
+# print(gauss(mean, cov, [0,0]))
+# print('derivative: ', gauss_derivative(gauss, mean, cov, [10,10]))
+# print("MEAN: ", mean)
+#
+# # Plot the gaussian curve and its derivative
+# l1 = []
+# l2 = []
+# lD = []
+# l0 = []
+# lw = []
+# for i in np.arange(5,15.25,0.25):
+#     v = np.array([i,i])
+#     l1.append(v)
+#     l2.append(gauss(mean, cov, v))
+#     l0.append(0)
+#     lD.append(gauss_derivative(gauss, mean, cov, v))
+#
+# plt.plot(l1, l2, "bo")
+# plt.plot(l1, lD, "r^")
+# plt.plot(l1, l0, "g-")
+# plt.show()
